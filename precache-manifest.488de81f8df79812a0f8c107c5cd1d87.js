e5531f4e087455fed299c40a59aca61bf77ecc7c
Celso Junior <clsjunnior@live.com>
2020-08-25 22:38:16 -0300

    Updates

---
+++ precache-manifest.488de81f8df79812a0f8c107c5cd1d87.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9e2b5db6a35909c3b01edfb40172d6",
+    "url": "/job-listings-frontendmentor/index.html"
+  },
+  {
+    "revision": "220fa6cbbb8e0c319a38",
+    "url": "/job-listings-frontendmentor/static/css/main.1efa5998.chunk.css"
+  },
+  {
+    "revision": "d71542d229903a773231",
+    "url": "/job-listings-frontendmentor/static/js/2.c51e7d9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listings-frontendmentor/static/js/2.c51e7d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220fa6cbbb8e0c319a38",
+    "url": "/job-listings-frontendmentor/static/js/main.c428c80c.chunk.js"
+  },
+  {
+    "revision": "7e1b75219bc10385309e",
+    "url": "/job-listings-frontendmentor/static/js/runtime-main.6a2162f0.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/job-listings-frontendmentor/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listings-frontendmentor/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/job-listings-frontendmentor/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/job-listings-frontendmentor/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/job-listings-frontendmentor/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/job-listings-frontendmentor/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/job-listings-frontendmentor/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/job-listings-frontendmentor/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/job-listings-frontendmentor/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/job-listings-frontendmentor/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/job-listings-frontendmentor/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
